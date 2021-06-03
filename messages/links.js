c78363cb0b3251a3bd2a9858244f5f5ceee14f53
Weoka <weokap@gmail.com>
2021-06-03 20:12:05 +0200

    New capital link addition

--- messages/links.js
+++ messages/links.js
@@ -6,7 +6,7 @@ module.exports = async (client, linksChannel) => {
 		const officialLinks = [
 			{ name: 'DOGEC Links', value: '[Website](https://dogec.io)\n[Explorer](https://explorer.dogec.io)\n[Wallet](https://github.com/dogecash/dogecash/releases)\n[Official mobile wallet](http://flits.dogec.io/)\n[Rewards calculator](https://dogecash.org/rewards/)', inline: true },
 			{ name: 'Social Media', value: '[Twitter](https://twitter.com/dogecashcoin/)\n[BitcoinTalk](https://bitcointalk.org/index.php?topic=5134905.0)\n[Discord](https://discord.gg/kHNKKYeBVS)\n[Telegram](https://t.me/dogecashcoin)\n[Github](https://github.com/dogecash)\n[Reddit](https://www.reddit.com/r/DogeCash)', inline: true },
-			{ name: 'Exchanges', value: '[Stakecube](http://stakecube.dogec.io/)\n[Stex](http://stex.dogec.io/)', inline: true },
+			{ name: 'Exchanges', value: '[Stakecube](http://stakecube.dogec.io/)\n[Stex](http://stex.dogec.io/)\n[new.capital](http://newcapital.dogec.io/)', inline: true },
 		];
 
 		const notOfficialLinks = [
