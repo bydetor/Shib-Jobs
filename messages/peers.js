a5234c892ba286f71c467cb0728e032b5737a5d3
Weoka <weokap@gmail.com>
2021-08-02 18:40:45 +0200

    [Peers] Add 10 peers max

--- messages/peers.js
+++ messages/peers.js
@@ -1,26 +1,37 @@
-const { createEmbed } = require('../utilities.js');
-const axios = require('axios');
+const { createEmbed } = require("../utilities.js");
+const axios = require("axios");
 
 module.exports = async (client, channel) => {
-	try {
-		const peers = (await axios.get('http://api.dogecash.org/api/v1/network/peers'))['data']['result'];
-		let textpeers = '';
+  try {
+    const peers = (
+      await axios.get("http://api.dogecash.org/api/v1/network/peers")
+    )["data"]["result"];
+    let textpeers = "";
 
-		for (const peer of peers) {
-			textpeers = textpeers +
-                `addnode=${peer}\n`;
-		}
+    for (let i = 0; i < peers.length; i++) {
+      textpeers = textpeers + `addnode=${peers[i]}\n`;
+      if (i >= 10) {
+        break;
+      }
+    }
 
-		const peersEmbed = [
-			// eslint-disable-next-line quotes
-			{ name: 'Add these lines to your dogecash.conf file:', value: "```" + textpeers + "```", inline: false },
-		];
+    const peersEmbed = [
+      // eslint-disable-next-line quotes
+      {
+        name: "Add these lines to your dogecash.conf file:",
+        value: "```" + textpeers + "```",
+        inline: false,
+      },
+    ];
 
-		const peerMessage = createEmbed('FILTERED AND REAL-TIME NON-FORKING PEERS!', peersEmbed, 'DOGEC • The Doge is Now the Master(node)');
+    const peerMessage = createEmbed(
+      "FILTERED AND REAL-TIME NON-FORKING PEERS!",
+      peersEmbed,
+      "DOGEC • The Doge is Now the Master(node)"
+    );
 
-		channel.send(peerMessage);
-	}
-	catch (e) {
-		console.log(e);
-	}
+    channel.send(peerMessage);
+  } catch (e) {
+    console.log(e);
+  }
 };
